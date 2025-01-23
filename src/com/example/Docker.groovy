7c6c8cc56a27ff79a86b9d2cae959f81cc91ce16
michaelanunda <101601723+michaelanunda@users.noreply.github.com>
2025-01-22 20:11:57 -0600

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,4 +1,35 @@
 #!/user/bin/env groovy
+
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName, String imageTag) {
+        script.echo "building the docker image..."
+        script.sh "docker build -t ${imageName}:${imageTag} ."
+    }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+        }
+    }
+
+    def dockerPush(String imageName, String imageTag) {
+        script.sh "docker push ${imageName}:${imageTag}"
+    }
+}
+
+
+
+
+/*
        package com.example
 
        class Docker implements Serializable {
@@ -18,33 +49,4 @@
                }
           }
        }
-
-
-
-/*
-package com.example
-
-class Docker implements Serializable {
-
-    def script
-
-    Docker(script) {
-        this.script = script
-    }
-
-    def buildDockerImage(String imageName) {
-        script.echo "building the docker image..."
-        script.sh "docker build -t $imageName ."
-        }
-
-    def dockerLogin() {
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-        }
-    }
-
-    def dockerPush(String imageName) {
-        script.sh "docker push $imageName"
-    }
-}
 */
