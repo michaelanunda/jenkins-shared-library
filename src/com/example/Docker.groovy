cbf3fa52bce1f384736a0b1eb999c987b0421c8e
michaelanunda <101601723+michaelanunda@users.noreply.github.com>
2025-04-29 21:29:41 -0500

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -27,6 +27,13 @@ class Docker implements Serializable {
         // script.sh "docker push ${imageName}:${imageTag}"
         script.sh "sudo docker push ${imageName}:${imageTag}"
     }
+
+    def dec2Deploy(String ec2Instance, String shellCmd) {
+        // script.sh "docker push ${imageName}:${imageTag}"
+        script.sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
+        script.sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user"
+        script.sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} '${shellCmd}'"
+    }
 }
 
 
