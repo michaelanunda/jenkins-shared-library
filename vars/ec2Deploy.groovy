ce20b8633c6dba29987ab2ca778cd0c351015cc1
michaelanunda <101601723+michaelanunda@users.noreply.github.com>
2025-04-29 22:21:10 -0500

    Update ec2Deploy.groovy

--- vars/ec2Deploy.groovy
+++ vars/ec2Deploy.groovy
@@ -1,9 +1,6 @@
 #!/user/bin/env groovy
-
-def call(String shellCmd, String ec2Instance) {
-    sh """
-        scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user
-        scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user
-        ssh -o StrictHostKeyChecking=no ${ec2Instance} "${shellCmd}"
-    """
-}
+ 
+ import com.example.Docker
+ def call(String ec2Instance, String shellCmd, String ec2Path) {
+     return new Docker(this).ec2Deploy(ec2Instance, shellCmd, ec2Path)
+ }
