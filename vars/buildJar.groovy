f000b61ae17fb98ffbd9f7d5438679b2005073b1
michaelanunda <101601723+michaelanunda@users.noreply.github.com>
2025-05-05 12:56:48 -0500

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
+import com.example.Docker
 def call() {
-    echo "building the application for branch ${BRANCH_NAME}"
-    sh 'mvn clean install'
+    return new Docker(this).buildJar()
 }
