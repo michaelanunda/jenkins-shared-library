1e24df6c977ffab0f23bc1b621fc708f511279a4
michaelanunda <101601723+michaelanunda@users.noreply.github.com>
2025-05-05 14:58:07 -0500

    Update commitAndPushChanges.groovy

--- vars/commitAndPushChanges.groovy
+++ vars/commitAndPushChanges.groovy
@@ -2,7 +2,5 @@
 
 import com.example.Docker
 def call(String pomVersion) {
-    withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_TOKEN')]) {
-    return new Docker(this).commitAndPushChanges(pomVersion)
-}
+     return new Docker(this).commitAndPushChanges(pomVersion)
  }
